3cde852351d4c78f68b688198bfdc3192e1bfa2d
ieko <mendo931411.mou@gmail.com>
2019-07-20 15:32:32 +0900

    ルーティングを設定

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -2,12 +2,32 @@ class TaskController < ApplicationController
   def top
   end
 
+  def index
+    @tasks = Task.all
+    @statuses = Status.all
+  end
+
   def setting
   end
 
-  def index
+  def show
   end
 
-  def show
+  def create
+    @task = Task.new(name: params[:task_name], status_id: 1)
+    @task.save
+
+    # @tasks = Task.all
+    # @tasks.each do |task|
+    # task.status_id=1
+    # end
+    @tasks.save
+
+    redirect_to("/task/index")
+  end
+
+  def reset
+    Task.find(1).destroy
+    redirect_to("/home/top")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  get 'task/top'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "task/top" => "task#top"
+  get "task/index" => "task#index"
+  get "task/setting" => "task#setting"
+  get "task/setting" => "task#show"
+
+  post "task/create" => "task#create"
+  post "task/reset" => "task#reset"
 end
