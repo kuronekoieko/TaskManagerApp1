7565ec139b92758b94599ce5424f0160512a863a
ieko <mendo931411.mou@gmail.com>
2019-07-24 01:11:22 +0900

    【タスク一覧画面】仮で作成

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,21 +1,32 @@
 # encoding: utf-8
 
 class TaskController < ApplicationController
+  protect_from_forgery
+
   def index
     @tasks = Task.all
     @pics_hush = {}
     for pic in Pic.all
       @pics_hush[pic.name] = pic.id
     end
+    puts "いんでっくす"
   end
 
   def create
-    @task = Task.new(name: params[:task_name])
+    puts "くりえいと"
+    @task = Task.new(
+      name: params[:task_name],
+      pic_id: 1,
+      status_id: 1,
+      priority_id: 1,
+      classification_id: 1,
+    )
     @task.save
     redirect_to("/task/index")
   end
 
   def delete
+    puts "でりーと"
     params[:deletelist].each do |di1, di2|
       puts di2
       if di2 == "1"
@@ -27,8 +38,19 @@ class TaskController < ApplicationController
   end
 
   def change
+    puts "ああああああああああああああああああああああああ"
     task = Task.find_by(id: params[:task_id])
-    task.update(pic_id: params[:pic_id])
+    # task.update(pic_id: params[:pic_id])
+    task.pic_id = params[:pic_id]
+
+    if task.save
+      puts "保存成功！！！！！！！！"
+    else
+      puts "保存できない！！！！！！！！"
+    end
+
+    # puts task.name
+    # puts "ああああああああああああああああああああああああ"
     redirect_to("/task/index")
   end
 end
