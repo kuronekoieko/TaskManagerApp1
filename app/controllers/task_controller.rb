93ca12f1e781a688a281d8e8eb1ad03cac0aabaa
ieko <mendo931411.mou@gmail.com>
2019-07-24 02:21:33 +0900

    【タスク一覧画面】パラメータの初期値を入れないように変更

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -5,33 +5,23 @@ class TaskController < ApplicationController
 
   def index
     @tasks = Task.all
-    puts "いんでっくす"
   end
 
   def create
-    puts "くりえいと"
     @task = Task.new(
       name: params[:task_name],
-      pic_id: 1,
-      status_id: 1,
-      priority_id: 1,
-      classification_id: 1,
     )
     @task.save
     redirect_to("/task/index")
   end
 
   def delete
-    puts "でりーと"
     params[:deletelist].each do |di1, di2|
       puts di2
       if di2 == "1"
         Task.find_by(id: di1).delete
       end
     end
-
     redirect_to("/task/index")
   end
-
- 
 end
